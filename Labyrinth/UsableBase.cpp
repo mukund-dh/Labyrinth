de2072eecac77e85cf900912a4cd7767bbd9a5bd
pixeltek-vitek <mukund.dh@outlook.com>
2015-04-20 16:11:45 +0530

    Added UsableBase

---
+++ Labyrinth/UsableBase.cpp
@@ -0,0 +1,8 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "Labyrinth.h"
+#include "UsableBase.h"
+
+
+
+
---
+++ Labyrinth/UsableBase.h
@@ -0,0 +1,19 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "UsableActor.h"
+#include "UsableBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class LABYRINTH_API AUsableBase : public AUsableActor
+{
+	GENERATED_BODY()
+	
+	
+	
+	
+};
